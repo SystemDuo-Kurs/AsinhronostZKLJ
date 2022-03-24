b97e0ecadb944b0b3d625955e977a7b4f71b55b6
Dragan Pepic <d.pepic@hotmail.com>
2022-03-24 17:43:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿await Brojac();
+Console.WriteLine("Pozvao prvi");
+Brojac();
+Console.WriteLine("Pozvao drugi");
+Brojac();
+Console.WriteLine("Pozvao ih sve");
+
+async Task Brojac()
+{
+    int broj = 10;
+    Random r = new();
+    while (broj > 0)
+    {
+        await Task.Delay(r.Next(2000));
+        Console.WriteLine(broj);
+        broj--;
+    }
+    Console.WriteLine("======FIN========");
+}
